6ef180c3c26f8b76ba8352cde50d0b8b7dacbbda
emiflake <emily.flakeheart@gmail.com>
2019-03-20 22:50:50 +0000

    minor header changes

--- libft.h
+++ libft.h
@@ -39,13 +39,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str); // TODO
-int		ft_isalpha(int c); // TODO
-int		ft_isdigit(int c); // TODO
-int		ft_isalnum(int c); // TODO
-int		ft_isascii(int c); // TODO
-int		ft_isprint(int c); // TODO
-int		ft_toupper(int c); // TODO
-int		ft_tolower(int c); // TODO
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 /*
 ** Section extra
