44d1a0311098926b24cb4ad7373071c9a6fa4a4f
Noe Martins <nmartins@f1r5s3.codam.nl>
2019-03-20 19:27:55 +0100

    header and more tests & functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,36 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_strstr.c                                        :+:    :+:            */
+/*   ft_atoi.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: nmartins <nmartins@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/20 18:08:03 by nmartins      #+#    #+#                 */
-/*   Updated: 2019/03/20 19:05:43 by nmartins      ########   odam.nl         */
+/*   Created: 2019/03/20 19:02:14 by nmartins      #+#    #+#                 */
+/*   Updated: 2019/03/20 19:26:49 by nmartins      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_strstr(const char *haystack, const char *needle)
+int		ft_atoi(const char *str)
 {
-	char *is;
-	char *js;
-	char *lookback;
 
-	is = (char*)haystack;
-	while (*is)
-	{
-		lookback = is;
-		js = (char*)needle;
-		while (*is && *js && *is == *js)
-		{
-			is++;
-			js++;
-		}
-		if (!*js)
-			return (lookback);
-		is = lookback + 1;
-	}
-	return (0);
 }
