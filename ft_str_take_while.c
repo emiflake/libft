6815a92b087b264fc152c218bf88a408f4e97e14
emiflake <emily.flakeheart@gmail.com>
2019-03-21 20:16:58 +0100

    tiny bugfix

--- ft_str_take_while.c
+++ ft_str_take_while.c
@@ -6,7 +6,7 @@
 /*   By: nmartins <nmartins@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/21 12:20:04 by nmartins      #+#    #+#                 */
-/*   Updated: 2019/03/21 12:20:09 by nmartins      ########   odam.nl         */
+/*   Updated: 2019/03/21 19:39:13 by nmartins      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include <stdlib.h>
 
-char	*ft_str_take_while(char *source, int (*f)(int))
+char	*ft_str_take_while(const char *source, int (*f)(int))
 {
 	size_t	i;
 	char	*out;
