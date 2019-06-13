f572b61765b7cdf8f5e271a52739d503d000e7c0
Noah Loomans <noahloomans@gmail.com>
2019-06-13 15:28:26 +0200

    fix: strn{cmp,equ} now function properly with n = 0

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   ft_strncmp.c                                       :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: nmartins <nmartins@student.codam.n>          +#+                     */
+/*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/20 18:34:04 by nmartins      #+#    #+#                 */
-/*   Updated: 2019/03/20 18:39:25 by nmartins      ########   odam.nl         */
+/*   Created: 2019/03/22 11:07:02 by nloomans       #+#    #+#                */
+/*   Updated: 2019/06/12 14:41:55 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*us1;
-	unsigned char	*us2;
+	size_t	i;
 
 	i = 0;
-	us1 = (unsigned char*)s1;
-	us2 = (unsigned char*)s2;
-	while (i < n && us1[i] && us2[i] && us1[i] == us2[i])
+	while (1)
 	{
+		if (i >= n || (s1[i] == '\0' && s2[i] == '\0'))
+			return (0);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if (i == n)
-		i--;
-	return (us1[i] - us2[i]);
 }
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_strnequ.c                                            :+:    :+:       */
+/*   ft_strnequ.c                                       :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: nmartins <nmartins@student.codam.n>          +#+                     */
+/*   By: nloomans <nloomans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/21 20:24:33 by nmartins      #+#    #+#                 */
-/*   Updated: 2019/04/01 19:26:06 by nmartins            ########   odam.nl   */
+/*   Created: 2019/03/27 11:29:08 by nloomans       #+#    #+#                */
+/*   Updated: 2019/06/12 14:58:23 by nloomans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int				ft_strnequ(char const *s1, char const *s2, size_t n)
+int		ft_strnequ(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
-
-	i = 0;
-	while (s1[i] && s2[i] && i < n)
-	{
-		if (s1[i] != s2[i])
-			return (0);
-		i++;
-	}
-	return (s1[i] == s2[i]);
+	return (ft_strncmp(s1, s2, n) == 0);
 }
